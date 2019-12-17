60974885491b54ec09f563c0e4e60cc1af528d98
Jonathon Chambers <FFCoder@users.noreply.github.com>
2019-12-17 16:33:55 -0500

    Barebones Operation is Working

--- src/js/app.js
+++ src/js/app.js
@@ -6,12 +6,19 @@ const child = require('child_process');
 const WINDOW_STATE = {
     NULL: {}
 }
+const WIN_MODES = {
+    PAPERCUT: "PAPERCUT",
+    VERITIME: "VERITIME",
+}
+
 const VERITIME_URL = "https://veritime.aesoponline.com/Clock?Org=10717&KID=1593"
 var MAX_IDLE_TIME = 6;
 const VERITIME_BUTTON = document.getElementById('vtLaunch');
 const PAPERCUT_BUTTON = document.getElementById('papercutLaunch');
+const PAPERCUT_CWD = "C:\\Users\\admin.jonathon\\Downloads\\Papercut\\PCRelease"
 
 let win;
+var WIN_MODE = null;
 
 const loadVeritime = () => {
     win = new BrowserWindow({
@@ -22,13 +29,37 @@ const loadVeritime = () => {
     // Veritime may have a longer idle time if user is preparing to use the barcode scanner. 
     // So I increased the idle time here. 
     MAX_IDLE_TIME = 10;
+    WIN_MODE = WIN_MODES.VERITIME;
 }
 
 const loadPapercut = () => {
     win = child.spawn("pc-release.exe", {
-        cwd: '\\\\papercut\\PCRelease\\',
+        cwd: PAPERCUT_CWD,
     });
+    WIN_MODE = WIN_MODES.PAPERCUT;
+    MAX_IDLE_TIME = 10;
     
+}
+
+const checkForActivity = () => {
+    var idleTime = powerMonitor.getSystemIdleTime();
+    if (idleTime >= MAX_IDLE_TIME) {
+        if (win !== WINDOW_STATE.NULL) {
+            switch (WIN_MODE) {
+                case WIN_MODES.PAPERCUT:
+                    win.kill();
+                    win = null;
+                    WIN_MODE = null;
+                    break;
+                case WIN_MODES.VERITIME:
+                    win.close();
+                    win = null;
+                    WIN_MODE = null;
+                    break;
+                
+            }
+        }
+    }
 }
 
 
@@ -36,12 +67,4 @@ const loadPapercut = () => {
 VERITIME_BUTTON.addEventListener('click', loadVeritime);
 PAPERCUT_BUTTON.addEventListener('click', loadPapercut);
 
-setInterval(() => {
-    var idleTime = powerMonitor.getSystemIdleTime();
-    if (idleTime >= MAX_IDLE_TIME) {
-        if (win !== WINDOW_STATE.NULL) {
-            win.close();
-            win = null;
-        }
-    }
-}, 500);
+setInterval(checkForActivity, 500);
