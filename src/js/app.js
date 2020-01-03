64862605a0dcd61bef34d003d809ed1c2dafec04
Jonathon Chambers <FFCoder@users.noreply.github.com>
2020-01-03 07:12:15 -0500

    Commit before bootstrap change.

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,10 @@ const { powerMonitor, BrowserWindow, app } = remote;
 const child = require('child_process');
 const path = require('path');
 
+const env = {
+    ...process.env,
+    NO_UPDATE_NOTIFIER: true,
+}
 
 
 const WINDOW_STATE = {
@@ -13,15 +17,24 @@ const WIN_MODES = {
     VERITIME: "VERITIME",
 }
 
-const VERITIME_URL = "https://veritime.aesoponline.com/Clock?Org=10717&KID=1593"
-var MAX_IDLE_TIME = 6;
+const VERITIME_URL = "https://veritime.aesoponline.com/Clock?Org=10717&KID=1593";
+// Trying out a max time of 10 seconds. Will adjust if needed.
+var MAX_IDLE_TIME = 10;
 const VERITIME_BUTTON = document.getElementById('vtLaunch');
 const PAPERCUT_BUTTON = document.getElementById('papercutLaunch');
-//const PAPERCUT_CWD = path.join(path.dirname(__dirname), "PCRelease");
 const PAPERCUT_CWD = path.join(`\\\\papercut\\`, 'PCRelease')
 
 let win;
 var WIN_MODE = null;
+
+const showWaitingMessage = (windowTimeout) => {
+    var main_win = document.getElementById('main_win');
+    var warning_alert = document.createElement('div');
+    warning_alert.classList.add('alert');
+    warning_alert.classList.add('alert-info');
+    warning_alert.innerHTML = "A test alert";
+    main_win.appendChild(warning_alert);
+}
 
 const loadVeritime = () => {
     win = new BrowserWindow({
@@ -29,21 +42,22 @@ const loadVeritime = () => {
     });
     win.loadURL(VERITIME_URL);
     win.show();
-    // Veritime may have a longer idle time if user is preparing to use the barcode scanner. 
-    // So I increased the idle time here. 
-    MAX_IDLE_TIME = 10;
     WIN_MODE = WIN_MODES.VERITIME;
 }
 
-const loadPapercut = async () => {
-    win = await child.spawn("pc-release.exe", {
+const loadPapercut = () => {
+    win = child.spawn("pc-release.exe", {
         cwd: PAPERCUT_CWD,
+        env: env
     });
     WIN_MODE = WIN_MODES.PAPERCUT;
-    MAX_IDLE_TIME = 10;
+    showWaitingMessage(5);
     
 }
 
+// Checks to see if screen has been inactive for MAX_IDLE_TIME(Variable) and 
+// if so, will close the running task.
+// TODO: Find a more elegant solution.
 const checkForActivity = () => {
     var idleTime = powerMonitor.getSystemIdleTime();
     if (idleTime >= MAX_IDLE_TIME) {
